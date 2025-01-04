82d7264239eb1abaea3e6c099fb439975f99d727
shama <shamarorostislav@gmail.com>
2025-01-05 02:02:41 +0300

    removing unnecessary code that is not the source of the problem

--- app/src/main/java/com/example/testtelephoto/ui/screens/DrawingScreen.kt
+++ app/src/main/java/com/example/testtelephoto/ui/screens/DrawingScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -27,10 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ScaleFactor
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -39,15 +35,12 @@ import androidx.compose.ui.platform.ViewConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
-import androidx.compose.ui.unit.times
 import coil.request.CachePolicy
 import coil.request.ImageRequest
 import com.example.testtelephoto.R
 import me.saket.telephoto.zoomable.ZoomableContentTransformation
 import me.saket.telephoto.zoomable.coil.ZoomableAsyncImage
 import me.saket.telephoto.zoomable.rememberZoomableImageState
-import kotlin.math.ceil
 import kotlin.random.Random
 
 
@@ -162,8 +155,6 @@ fun DrawingScreen() {
                             createLine(
                                 appCoordinatesFirst = Coordinates(frX, frY),
                                 appCoordinatesLast = Coordinates(scX, scY),
-                                containerSizePx = containerSizePx,
-                                containerSizeDp = containerSizeDp,
                                 transformation = transformation,
                                 strokeWidthForLine = strokeWidthForLine,
                                 color = Color.Red
@@ -181,8 +172,6 @@ fun DrawingScreen() {
 fun createLine(
     appCoordinatesFirst: Coordinates,
     appCoordinatesLast: Coordinates,
-    containerSizePx: IntSize,
-    containerSizeDp: Pair<Dp, Dp>,
     transformation: ZoomableContentTransformation,
     strokeWidthForLine: Float,
     color: Color,
@@ -194,39 +183,6 @@ fun createLine(
     val frY = (appCoordinatesFirst.y)
     val scX = (appCoordinatesLast.x)
     val scY = (appCoordinatesLast.y)
-    val (minX, maxX) = if (frX < scX) frX to scX else scX to frX
-    val (minY, maxY) = if (frY < scY) frY to scY else scY to frY
-    val width = maxX - minX
-    val height = maxY - minY
-    val widthDp = with(LocalDensity.current) { width.toDp() }
-    val heightDp = with(LocalDensity.current) { height.toDp() }
-    val necessaryWidth = if (widthDp <= 0.dp) 30.dp else widthDp
-    val necessaryHeight = if (heightDp <= 0.dp) 30.dp else heightDp
-    val maxBoxWidth = containerSizeDp.first
-    val maxBoxHeight = containerSizeDp.second
-    val numBoxesX = ceil(necessaryWidth / maxBoxWidth).toInt()
-    val numBoxesY = ceil(necessaryHeight / maxBoxHeight).toInt()
-    for (boxX in 0 until numBoxesX) {
-        for (boxY in 0 until numBoxesY) {
-            val currentMinX = minX + boxX * containerSizePx.width
-            val currentMinY = minY + boxY * containerSizePx.height
-            val currentWidth =
-                min(maxBoxWidth, necessaryWidth - boxX * maxBoxWidth)
-            val currentHeight =
-                min(maxBoxHeight, necessaryHeight - boxY * maxBoxHeight)
-            Box(
-                modifier = defectModifier(
-                    scale = scale,
-                    minX = currentMinX,
-                    minY = currentMinY,
-                    offsetX = offsetX,
-                    offsetY = offsetY,
-                    width = currentWidth,
-                    height = currentHeight
-                )
-            )
-        }
-    }
     Canvas(modifier = Modifier) {
         val startX =
             (frX * scale.scaleX) + offsetX
@@ -255,31 +211,3 @@ data class Coordinates(
     val x: Float,
     val y: Float,
 )
-
-@Composable
-fun defectModifier(
-    scale: ScaleFactor,
-    minX: Float,
-    minY: Float,
-    offsetX: Float,
-    offsetY: Float,
-    width: Dp,
-    height: Dp,
-): Modifier {
-    val modifier = Modifier
-        .graphicsLayer {
-            scaleX = scale.scaleX
-            scaleY = scale.scaleY
-            translationX =
-                (minX * scale.scaleX) + offsetX
-            translationY =
-                (minY * scale.scaleY) + offsetY
-            transformOrigin = TransformOrigin(0f, 0f)
-        }
-        .size(
-            width = width,
-            height = height
-        )
-
-    return modifier
-}
